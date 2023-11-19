96f10baf19d1e63157a6721e70c7d267e560176c
litoninot <litoninot12345@gmail.com>
2023-11-19 14:19:28 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache; 
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("/",function() {
+    // Cache::store('redis');
+
+    $value = Cache::remember('users',now()->addDays(1),function(){
+        return \App\Models\User::get();
+    });
+
+    return response()->json($value);
 });
